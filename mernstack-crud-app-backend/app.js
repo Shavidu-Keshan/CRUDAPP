af51d359daaf1e4f6fb1f1c1a3beb15767e46f5e
Shavidu Keshan <shavidu4321@gmail.com>
2024-08-20 11:30:11 +0530

    create data display part user details page

--- mernstack-crud-app-backend/app.js
+++ mernstack-crud-app-backend/app.js
@@ -4,11 +4,12 @@ const mongoose = require('mongoose');
 const router = require("./Route/UserRoutes");
 
 const app = express();
-
+const cors = require("cors");
 
 
 //Middleware
 app.use(express.json());
+app.use(cors());
 app.use("/users",router);
 
 mongoose.connect("mongodb+srv://keshanshavidu:Shavindu2000@shavindu9.1gh7s13.mongodb.net/?retryWrites=true&w=majority&appName=Shavindu9")
